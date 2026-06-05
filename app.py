8979f16460738c69879daa5eda095f0fc7097343
Arwa Alasmari <arwaalasmr@gmail.com>
2026-06-05 22:27:58 +0300

    Added TOTP generation page

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ import hashlib
 import pyotp
 import qrcode
 
-from database import create_database, add_user
+from database import (
+
+    create_database,
+
+    add_user,
+
+    get_secret_key
+
+)
 
 app = Flask(__name__)
 
@@ -39,14 +47,39 @@ def register():
         )
 
         return f"""
-        <h2>User Registered Successfully</h2>
+          <h2>User Registered Successfully</h2>
 
-        <p>Scan this QR Code using Google Authenticator</p>
+          <p>Scan this QR Code using Google Authenticator</p>
 
-        <img src="/static/{username}_qr.png" width="250">
-        """
+          <img src="/static/{username}_qr.png" width="250" alt="QR code for two-factor authentication setup with {username} account on CryptographyProject. Scan this code using Google Authenticator or similar authenticator app to enable TOTP-based login security.">
 
+          <br><br>
+
+          <a href="/totp/{username}">
+           View Generated TOTP
+          </a>
+          """
     return render_template("register.html")
+
+@app.route("/totp/<username>")
+def show_totp(username):
+
+    secret_key = get_secret_key(username)
+
+    if secret_key is None:
+        return "<h2>User Not Found</h2>"
+
+    totp = pyotp.TOTP(secret_key)
+
+    current_code = totp.now()
+
+    return f"""
+    <h2>Generated TOTP Code</h2>
+
+    <p>Username: {username}</p>
+
+    <h3>Current OTP: {current_code}</h3>
+    """
 
 
 if __name__ == "__main__":
